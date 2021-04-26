01599a0f2a74bd978cbef8fda29d97ec108113b8
Bennett Bennett <rbennett@quantros.com>
2021-04-26 11:30:45 -0400

    Adding logging

--- RaccoonJail/Api/Controllers/InmateCrudController.cs
+++ RaccoonJail/Api/Controllers/InmateCrudController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading.Tasks;
 using Database.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Models;
 using Models.Dtos;
 using Models.Requests;
@@ -11,21 +12,25 @@ namespace Api.Controllers
     public class InmateCrudController : ControllerBase
     {
         private readonly IInmateCrudService _inmateCrudService;
+        private readonly ILogger<InmateCrudController> _logger;
 
-        public InmateCrudController(IInmateCrudService inmateCrudService)
+        public InmateCrudController(IInmateCrudService inmateCrudService, ILogger<InmateCrudController> logger)
         {
             _inmateCrudService = inmateCrudService;
+            _logger = logger;
         }
 
         [HttpPost("AddInmate")]
         public async Task<ActionResult<long>> AddInmate(string name, decimal size, ArrestLocation arrestLocation, HungerLevel hungerLevel, HappinessLevel happinessLevel)
         {
+            _logger.LogInformation("I just got an AddInmate request");
             return await _inmateCrudService.AddInmateAndReturnId(name, size, arrestLocation, hungerLevel, happinessLevel);
         }
 
         [HttpDelete("DeleteInmate/{inmateId:long}")]
         public async Task<ActionResult> DeleteInmate(long inmateId)
         {
+            _logger.LogInformation("I just got a DeleteInmate request for {id}", inmateId);
             await _inmateCrudService.DeleteInmate(inmateId);
             return Ok();
         }
@@ -33,12 +38,14 @@ namespace Api.Controllers
         [HttpGet("GetInmate/{inmateId:long}")]
         public async Task<ActionResult<InmateDto>> GetInmate(long inmateId)
         {
+            _logger.LogInformation($"I just got a GetInmate request for {inmateId}");
             return await _inmateCrudService.ReadInmate(inmateId);
         }
 
         [HttpPatch("UpdateInmate")]
         public async Task<ActionResult> GetInmate([FromBody] InmateUpdateRequest inmateUpdateRequest)
         {
+            _logger.LogInformation("I just got a GetInmate request for {inmateId}", inmateUpdateRequest.Id);
             await _inmateCrudService.UpdateInmate(inmateUpdateRequest);
             return Ok();
         }
